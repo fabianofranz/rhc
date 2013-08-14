1feb86de489ff2f8c81aee1eae791e1ae3ec2836
fabianofranz <contact@fabianofranz.com>
2013-08-14 15:46:46 -0300

    Removed feature tests from env vars for now

--- features/step_definitions/env_steps.rb
+++ features/step_definitions/env_steps.rb
@@ -1,14 +1,1 @@
 include RHCHelper
-
-When /^'rhc env (\S+)( .*?)?'(?: command)? is run$/ do |subcommand, rest|
-  if subcommand =~ /^(set|unset|list|show)$/
-    Env.send subcommand.to_sym, rest
-    @env_output = Env.env_output
-    @exitcode = Env.exitcode
-  end
-end
-
-When /^a new environment variable "(.*?)" is set with value "(.*)"$/ do |name, value|
-  step "'rhc env set #{name}=#{value}' is run"
-end
-
